0de34c4d05d9378b0642840750aa2d4e1878b373
Abdullah <m26abdullah@gmail.com>
2021-03-02 15:49:43 -0800

    uselesss: merge rule testing

--- code/app/src/main/java/com/cmput301w21t06/crowdfly/MainActivity.java
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // My name is Abdullah, I am working on CrowdFly
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
