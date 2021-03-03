a92ee05ab7eb8844a84ace1dfa608d147611c542
Jin Lee <jl6@ualberta.ca>
2021-03-03 16:18:43 -0700

    made view triallog

---
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/ViewTrialLogActivity.java
@@ -0,0 +1,19 @@
+package com.cmput301w21t06.crowdfly.Views;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cmput301w21t06.crowdfly.R;
+
+public class ViewTrialLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_trial_log);
+
+
+    }
+}
