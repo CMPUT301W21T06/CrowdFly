4be14691b5d446ac697365a3d43646dd4905cbb4
Adam Elamy <adamelamy1@gmail.com>
2021-03-03 18:18:53 -0700

    fixed gradle-firestore bug

--- code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/MainActivity.java
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/MainActivity.java
@@ -7,16 +7,18 @@ import android.os.Bundle;
 
 import com.cmput301w21t06.crowdfly.R;
 import com.cmput301w21t06.crowdfly.Views.ViewExperimentLogActivity;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     // My name is Abdullah, I am working on CrowdFly
-
+    private FirebaseFirestore db;
 
     //my name is Haris :p
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
