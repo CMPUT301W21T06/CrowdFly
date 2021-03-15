7b1d12c26e3024e8a7d345ea3a17e6e04232accc
= <mharis2@ualberta.ca>
2021-03-14 22:13:05 -0600

    some ui design changes in AddExperimentActivity and its corresponding XML file

--- code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/AddExperimentActivity.java
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/AddExperimentActivity.java
@@ -2,9 +2,11 @@ package com.cmput301w21t06.crowdfly.Views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,30 +22,81 @@ public class AddExperimentActivity extends AppCompatActivity {
     EditText etMinNumTrials;
     EditText etRegion;
 
+    Button btnMeasurement;
+    Button btnBinomial;
+    Button btnCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_experiment);
         ExperimentLog experimentLog = ExperimentLog.getExperimentLog();
 
-        btnAddExperiment = findViewById(R.id.add_experiment);
-        btnCancel = findViewById(R.id.cancel_add);
-        etDescription = findViewById(R.id.add_description);
-        etMinNumTrials = findViewById(R.id.add_minTrials);
-        etRegion = findViewById(R.id.add_region);
+        //btnAddExperiment = findViewById(R.id.add_experiment);
+        btnCancel = findViewById(R.id.cancel_btn);
+        //etDescription = findViewById(R.id.trial_listview);
+        etMinNumTrials = findViewById(R.id.min_trial_edit_text);
+        etRegion = findViewById(R.id.region_edit_text);
 
-        btnAddExperiment.setOnClickListener(new View.OnClickListener() {
+        btnMeasurement = findViewById(R.id.m_btn);
+        btnBinomial = findViewById(R.id.binomial_btn);
+        btnCount = findViewById(R.id.count_btn);
+
+//        btnAddExperiment.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // need to add owner name later when we have users
+//
+//                final String description = etDescription.getText().toString();
+//                final String minNumTrials = etMinNumTrials.getText().toString();
+//                final String region = etRegion.getText().toString();
+//
+//                experimentLog.addExperiment(new Experiment(description, region, Integer.parseInt(minNumTrials)));
+//
+//                startActivity(new Intent(AddExperimentActivity.this, ViewExperimentLogActivity.class));
+//            }
+//        });
+
+        btnBinomial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // need to add owner name later when we have users
-
-                final String description = etDescription.getText().toString();
-                final String minNumTrials = etMinNumTrials.getText().toString();
-                final String region = etRegion.getText().toString();
-
-                experimentLog.addExperiment(new Experiment(description, region, Integer.parseInt(minNumTrials)));
-
+                //hard coding variables for now will be adjusted
+                final String description = "Binomial";
+                String region = getRegion();
+                int minNumTrials = getMinNumTrials();
+                experimentLog.addExperiment(new Experiment(description,region,minNumTrials));
                 startActivity(new Intent(AddExperimentActivity.this, ViewExperimentLogActivity.class));
+            }
+        });
+
+        btnMeasurement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String description = "Measurement";
+                String region = getRegion();
+                int minNumTrials = getMinNumTrials();
+                experimentLog.addExperiment(new Experiment(description,region,minNumTrials));
+                startActivity(new Intent(AddExperimentActivity.this, ViewExperimentLogActivity.class));
+            }
+        });
+
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String description = "Count";
+                String region = getRegion();
+                int minNumTrials = getMinNumTrials();
+
+                Log.d("myTag", region);
+
+                if (region.equals("")){
+                    makeToast("no",0);
+                }
+                else {
+                    experimentLog.addExperiment(new Experiment(description, region, minNumTrials));
+                    startActivity(new Intent(AddExperimentActivity.this, ViewExperimentLogActivity.class));
+                }
+
             }
         });
 
@@ -54,4 +107,22 @@ public class AddExperimentActivity extends AppCompatActivity {
             }
         });
     }
+
+    public String getRegion(){
+        return etRegion.getText().toString();
+    }
+    public Integer getMinNumTrials(){
+        String minNumTrials = etMinNumTrials.getText().toString();
+        return Integer.parseInt(minNumTrials);
+    }
+    public void makeToast(String toast, int n) {
+        //0 for short toast, 1 for long toast
+        if (n == 0) {
+            Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
