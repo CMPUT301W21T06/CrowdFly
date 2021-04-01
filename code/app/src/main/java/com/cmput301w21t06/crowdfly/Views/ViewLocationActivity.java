31798a56d8b7ae31abdebf33d86c1b57ad9a2962
Adam Elamy <adamelamy1@gmail.com>
2021-04-01 07:08:22 -0600

    adjusted location layout

--- code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/ViewLocationActivity.java
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/ViewLocationActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cmput301w21t06.crowdfly.R;
+import com.google.android.gms.maps.MapView;
 
 public class ViewLocationActivity extends AppCompatActivity {
-
+    private final String SELECTION = "COM.CMPUT301W21T06.CROWDFLY.ALL";
+    MapView map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_location);
+        setUpViews();
+    }
+
+
+    public void setUpViews(){
+        map = findViewById(R.id.mapView);
+
     }
 }
