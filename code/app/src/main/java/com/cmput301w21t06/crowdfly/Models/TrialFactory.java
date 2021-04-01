7888ad56f1d748661e94db6a3b2fa9025fe8a5f3
Qasim Khawaja <q.khawaja@yahoo.com>
2021-03-31 20:44:29 -0600

    refactor: use factory method to instanciate trial

---
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Models/TrialFactory.java
@@ -0,0 +1,35 @@
+package com.cmput301w21t06.crowdfly.Models;
+
+import java.util.Map;
+
+/**
+ * This class aims to contain the logic for checking trial type and instanciating the correct
+ * Trial Class
+ */
+
+public class TrialFactory {
+
+    public TrialFactory() {
+    }
+
+    /**
+     * Factory method for getting Trial
+     * @param trialType String representation of the trial Type
+     * @param metadata HashMap with metadata from the database
+     * @return Trial
+     * @throws Exception
+     */
+    public Trial getTrial(String trialType, Map<String, Object> metadata) throws Exception {
+        if(trialType.equals("binomial")){
+            return new BinomialTrial(metadata);
+        }
+        if(trialType.equals("count")){
+            return new CountTrial(metadata);
+        }
+        if(trialType.equals("measurement")){
+            return new MeasurementTrial(metadata);
+        }
+        throw new Exception("Not a valid trial type");
+    }
+
+}
---
+++ code/app/src/test/java/com/cmput301w21t06/crowdfly/Models/TrialFactoryTest.java
@@ -0,0 +1,5 @@
+import junit.framework.TestCase;
+
+public class TrialFactoryTest extends TestCase {
+
+}
