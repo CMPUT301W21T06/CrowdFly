f2696da69a12d7e203539c11b40322c04f1b4c46
Qasim Khawaja <q.khawaja@yahoo.com>
2021-04-02 15:07:07 -0600

    feat: access camera
    Signed-off-by: Qasim Khawaja <q.khawaja@yahoo.com>

--- code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/ScanCodeActivity.java
+++ code/app/src/main/java/com/cmput301w21t06/crowdfly/Views/ScanCodeActivity.java
@@ -1,15 +1,68 @@
+/**
+ * Referenced the following codelab from google to understand how to use camerax
+ * https://codelabs.developers.google.com/codelabs/camerax-getting-started#1
+ * Author: Google Open Source
+ */
+
 package com.cmput301w21t06.crowdfly.Views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
 import com.cmput301w21t06.crowdfly.R;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 
 public class ScanCodeActivity extends AppCompatActivity {
-
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_code);
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE,
+                                Barcode.FORMAT_AZTEC)
+                        .build();
+        previewView = findViewById(R.id.viewFinder);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (Exception e ) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, 100);
+            }
+        }, ContextCompat.getMainExecutor(this));
     }
-}
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+    }
+
+
+    }
